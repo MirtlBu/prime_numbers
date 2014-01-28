da21708330b982bef53bba43975721def1eeefc8
Mirtl <bubuzoglo@gmail.com>
2014-01-28 10:46:59 +0400

    add check for positive

--- test.js
+++ test.js
@@ -1,35 +1,51 @@
 function check(operation){
-        var inp = Number(document.getElementById("inp").value);
-        if (isNaN(inp)){
-            document.getElementById("message").innerHTML = "Input a number";
-        }
-        else{
-            document.getElementById("message").innerHTML = "";
-            if(operation === "increment"){
-                inp += 1;
-                isPrime(inp);
-                document.getElementById("inp").value = inp;
+    var get_message = document.getElementById("message");
+    var get_input = document.getElementById("inp");
+    var inp = Number(get_input.value);
+    if (isNaN(inp)){
+        get_message.innerHTML = "Input a number";
+    }
+    else{
+        get_message.innerHTML = "";
+        if(operation === "increment"){
+            inp += 1;
+            get_input.value = inp;
+            if(isPrime(inp) && isPositive(inp)){
+                get_message.innerHTML = "It's a prime number.";
             }
-            else if(operation === "decrement"){
-                inp -= 1;
-                isPrime(inp);
-                document.getElementById("inp").value = inp;
+            else{
+                get_message.innerHTML = "It's not a prime number.";
             }
         }
+        else if(operation === "decrement"){
+            inp -= 1;
+            get_input.value = inp;
+            if(isPrime(inp) && isPositive(inp)){
+                get_message.innerHTML = "It's a prime number.";
+            }
+            else{
+                get_message.innerHTML = "It's not a prime number.";
+            }
+        }
+    }
 }
 function isPrime(number){
     for(var i = 2; i < number; i++){
-        if((number % i) === 0){
-            document.getElementById("message").innerHTML = "It's not a prime number.";
-            return;
+        if(number % i === 0){
+            return false;
         }
     }
-    document.getElementById("message").innerHTML = "It's a prime number.";
+    return true;
+}
+function isPositive(number){
+    if(number > 0){
+        return true;
+    }
 }
 function plus(){
-        check("increment");
+    check("increment");
 }
 function minus(){
-        check("decrement");
+    check("decrement");
 }
 
